c68d88678c3ccecd174fba1118a6e5c0473aaa4b
Jose-PC\Jose <josedrlaptop@gmail.com>
2021-01-23 11:17:45 -0500

    Cambios en los template

--- MKL02Z32xxx4_Project1/sdk_hal/sdk_hal_uart0.c
+++ MKL02Z32xxx4_Project1/sdk_hal/sdk_hal_uart0.c
@@ -1,5 +1,5 @@
 /*! @file : sdk_hal_uart0.c
- * @author  Ernesto Andres Rincon Cruz
+ * @author  Jose Alberto Florez Martinez
  * @version 1.0.0
  * @date    8/01/2021
  * @brief   Driver para UART0
